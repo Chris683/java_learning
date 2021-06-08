b4e84e251be8c2eb46c3e910b7aeffcf2fa9e53c
Chris683 <wukang683@163.com>
2021-06-08 22:16:07 +0800

    Java基础知识

---
+++ java基础/src/java_grammar.java
@@ -0,0 +1,42 @@
+/**
+ * @author Chris
+ * @create 2021-06-08-21:20
+ */
+
+//1 Java特点
+/**
+ * 简单性
+ * 面向对象
+ * 分布式
+ * 健壮性
+ * 安全性
+ * 体系结构中立
+ * 可移植性
+ * 解释性
+ * 高性能性
+ * 多线程
+ * 动态性
+ */
+
+// 2 第一个Java程序
+// public class 后面采用的类名和文件名保持一样，一个Java程序里面只有一个public class；
+// class后面类名必须以字母开头，后面可以跟字母和数字的任意组合；
+// System.out.println（）输出数据后换行；  System.out.print()输出数据；
+public class java_grammar {
+    public static void main(String[] args) {
+        System.out.println("hello word!");
+        System.out.print("hello");
+        System.out.print(" word！");
+    }
+}
+
+// 3 注释
+// 单行注释
+/*多行
+注释*/
+/**
+ * 文档注释
+ */
+
+// 4 数据类型
+
